3b170d1cc0f0c72b7927eb569e08ba0168d31cb1
Nitish Yadav <nitishy01@gmail.com>
2022-03-19 00:35:12 +0530

    remove uglify.js

--- columnate.js
+++ columnate.js
@@ -1,5 +1,4 @@
 function Columnate() {
-  // apply columnate stylesheets to document
   var LoadStylesheet = function (url) {
     var c = document.createElement('link');
     c.rel = 'stylesheet';
@@ -7,27 +6,22 @@ function Columnate() {
     c.media = 'all';
     c.href = url;
     document.getElementsByTagName('head')[0].appendChild(c);
-  }
+  };
 
-  // callback that will replace document content with readable version
   var MakeReadable = function () {
     var doclone = document.cloneNode(true);
     var article = new Readability(doclone).parse();
-    // strip stray styling from the html tag itself
     var htmltag = document.getElementsByTagName("html")[0];
     htmltag.removeAttribute("class");
     htmltag.removeAttribute("style");
-    // reset head to nothing but our stylesheets
     document.head.innerHTML = '<meta name="viewport" content="width=device-width, initial-scale=1">';
     LoadStylesheet('//nityy.github.io/Unclutter/appearance.css');
     document.title = article.title;
-    // reset body html to nothing but reformatted content  
     document.body.removeAttribute("class");
     document.body.removeAttribute("style");
     document.body.innerHTML = '<h1>' + article.title + '</h1><div id="author">' + article.byline + '</div><hr>' + article.content;
   };
 
-  // load readability script and set it to be applied when loaded
   cmjs = document.createElement('script');
   cmjs.type = 'text/javascript';
   cmjs.src = '//nityy.github.io/Unclutter/readability/Readability.js';
--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 function CreateLink() {
-
-  // cribbed from bookmarkletify
-  var toplevel_ast = UglifyJS.parse(Columnate.toString() + "Columnate();");
-  toplevel_ast.figure_out_scope();
-  var compressor = UglifyJS.Compressor();
-  var compressed_ast = toplevel_ast.transform(compressor);
-  compressed_ast.figure_out_scope();
-
-  var url = "javascript: (function () {" + compressed_ast.print_to_string() + "}())";
+  var url = "javascript: (function () {" + Columnate.toString() + "Columnate();}())";
   document.getElementById("bookmarkletLink").href = url;
   document.getElementById("bookmarkletLink").className = "ready";
 };
